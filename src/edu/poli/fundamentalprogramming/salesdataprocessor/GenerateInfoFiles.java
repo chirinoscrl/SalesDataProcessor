a3c62f797cb65d799a43f2354d9403545f2483f3
Rosanny Chirinos <chirinoscrl@gmail.com>
2025-03-21 21:53:56 -0500

    Initial commit

---
+++ src/edu/poli/fundamentalprogramming/salesdataprocessor/GenerateInfoFiles.java
@@ -0,0 +1,48 @@
+package edu.poli.fundamentalprogramming.salesdataprocessor;
+
+/**
+ * Main class for generating test files for sales data processing.
+ * This class creates pseudo-random data files that will serve as input
+ * for the main program. It generates three types of files:
+ * 1. Sales files for each salesperson
+ * 2. A product information file
+ * 3. A salesperson information file
+ * 
+ * First deliverable for the Fundamental Programming Concepts module.
+ * 
+ */
+public class GenerateInfoFiles {
+    
+    
+    /**
+     * Main method that executes the file generation process.
+     * Creates test files with pseudo-random data for sales, products, and salespeople.
+     * 
+     * @param args Command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    /**
+     * Creates a sales file for a salesperson with pseudo-random data.
+     */
+    public static boolean createSalesMenFile(int randomSalesCount, String name, long id, String documentType) {
+		return true;
+    	
+    }
+    
+    /**
+     * Creates a file with pseudo-random product information.
+     */
+    public static boolean createProductsFile(int productsCount) {
+		return true;
+    }
+
+	/**
+     * Creates a file with pseudo-random salesperson information.
+     */
+    public static boolean createSalesManInfoFile(int salesmanCount) {
+    	return true;
+    }
+}
